7c670ee177d37b49b6714a83ed438ef4a0b06bd2
Stephen Colegrove <stevegc9@gmail.com>
2019-11-30 15:30:46 +0000

    level menu fun

--- source/MenuController.java
+++ source/MenuController.java
@@ -1,6 +1,8 @@
 import javafx.animation.AnimationTimer;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
@@ -13,12 +15,15 @@ import javafx.stage.Stage;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URL;
 
 public class MenuController {
 
     private static final double GAME_WIDTH = ((GameState.VIEW_RADIUS * 2) * (GameState.TILE_RES)) - GameState.TILE_RES * 2;
     private static final double GAME_HEIGHT = ((GameState.VIEW_RADIUS + 2) * (GameState.TILE_RES * 2));
     private static final int GAME_MARGIN = 0;
+    private static final String FAIL_VIEW_PATH = "./view/levelFail.fxml";
+    private static final String MENU_VIEW_PATH = "./view/menu.fxml";
 
     private GameState gs;
     private Scene scene;
@@ -33,6 +38,10 @@ public class MenuController {
      *
      */
     public void ngButton() {
+        loadLevel();
+    }
+
+    private void loadLevel() {
         gs = new GameState(stringFromFile(getClass().getResource("./map/map3.txt").getPath()));
         BorderPane root = new BorderPane();
         scene = new Scene(root, GAME_WIDTH, GAME_HEIGHT, Color.BLACK);
@@ -62,12 +71,17 @@ public class MenuController {
             if (gs.getCurrentState() == GameState.State.RUNNING) {
                 gs.update(event.getCode());
                 gs.drawRadius(gc, false);
-            } else if (gs.getCurrentState() == GameState.State.LOSE
-                    || gs.getCurrentState() == GameState.State.WIN) {
+            } else if (gs.getCurrentState() == GameState.State.WIN) {
                 if (event.getCode() == KeyCode.ESCAPE) {
                     mainStage.setScene(scene);
                 } else {
                     gs.restart();
+                }
+            } else if (gs.getCurrentState() == GameState.State.LOSE) {
+                try {
+                    levelFailMenu();
+                } catch (IOException e) {
+                    
                 }
             }
         });
@@ -95,6 +109,29 @@ public class MenuController {
             return null;
         }
     }
+
+    public void levelFailMenu() throws IOException {
+        Stage stage = new Stage();
+        FXMLLoader ld = new FXMLLoader();
+        URL url = getClass().getResource(FAIL_VIEW_PATH);
+        ld.setLocation(url);
+        Parent root = ld.load();
+
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+    public void mainMenu() throws IOException {
+        Stage stage = new Stage();
+        FXMLLoader ld = new FXMLLoader();
+        URL url = getClass().getResource(MENU_VIEW_PATH);
+        ld.setLocation(url);
+        Parent root = ld.load();
+
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
     public void quitGame() {
         Platform.exit();
     }
